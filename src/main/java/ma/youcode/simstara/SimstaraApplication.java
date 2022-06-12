f6157d454e83c56f089b6514c32f0b7fb7a2fbd3
JAMAL EDDINE ELIDRISSI Yassir <67668097+JAMALEEY@users.noreply.github.com>
2022-06-12 18:17:11 +0100

    first commit

---
+++ src/main/java/ma/youcode/simstara/SimstaraApplication.java
@@ -0,0 +1,13 @@
+package ma.youcode.simstara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimstaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimstaraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/youcode/simstara/SimstaraApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.youcode.simstara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimstaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
